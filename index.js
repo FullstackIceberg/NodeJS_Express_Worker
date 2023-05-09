e3fcf53f80a800854c5a1b47bca20739d0b2adea
kevin <pompitzgeneral@zentry.kr>
2023-05-09 13:48:25 +0900

    NodeJS WorkerThread 예제코드 작성

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { Worker } from 'worker_threads';
+const app = express();
+const port = 3000;
+
+app.get('/fibonacci/:n', (req, res) => {
+    console.log(`start`);
+    const n = parseInt(req.params.n);
+    console.log(`Get /fibonacci/${n}`);
+    // 워커 스레드를 통해 CPU 집약적인 작업을 백그라운드에서 처리하고, 메인 스레드에서 다른 요청을 병렬적으로 처리할 수 있음.
+    const worker = new Worker('./worker.js');
+    worker.on('message', (result) => {
+        console.log(`end`);
+        return res.send(`Fibonacci(${n}) = ${result}`);
+    });
+
+    console.log(`worker.postMessage(${n})`);
+    worker.postMessage(n);
+});
+
+app.get('/ping', (req, res) => {
+    console.log(`Get /ping`);
+    res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ worker.js
@@ -0,0 +1,12 @@
+import { parentPort } from 'worker_threads';
+
+function fibonacci(n) {
+    if (n <= 1) return n;
+    // console.log(`current n = ${n}`);
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+parentPort.on('message', (n) => {
+    const result = fibonacci(n);
+    parentPort.postMessage(result);
+});
